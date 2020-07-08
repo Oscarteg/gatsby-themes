6bbb07dbffed1b30e79ff93671e27d02ee398b0e
Oscar te Giffel <oscar@tegiffel.com>
2020-07-08 03:32:34 +0200

    Added build time to theme core

--- core/gatsby-theme-core/gatsby-node.js
+++ core/gatsby-theme-core/gatsby-node.js
@@ -1,4 +1,6 @@
 const fs = require("fs");
+const format = require("date-fns/format");
+
 const withDefaults = require(`./theme-options.ts`);
 
 exports.onPreBootstrap = ({ reporter }, themeOptions) => {
@@ -9,4 +11,35 @@ exports.onPreBootstrap = ({ reporter }, themeOptions) => {
     reporter.warn(`The ${imagesPath} directory is missing. Creating it now...`);
     fs.mkdirSync(imagesPath, { recursive: true });
   }
+};
+
+exports.sourceNodes = (
+  { actions, createNodeId, createContentDigest },
+  { formatAsDateString = true, formatting = { format: "PPP", utc: false } }
+) => {
+  const { createNode } = actions;
+
+  const buildDateData = {
+    currentDate: formatAsDateString
+      ? format(new Date(), formatting.format, formatting.utc)
+      : new Date(),
+  };
+
+  const buildDateNodeContent = JSON.stringify(buildDateData);
+
+  const buildDateNodeMeta = {
+    id: createNodeId(`current-build-date`),
+    parent: null,
+    children: [],
+    internal: {
+      type: `CurrentBuildDate`,
+      mediaType: `text/html`,
+      content: buildDateNodeContent,
+      contentDigest: createContentDigest(buildDateData),
+    },
+  };
+
+  const buildDateNode = { ...buildDateData, ...buildDateNodeMeta };
+
+  createNode(buildDateNode);
 };
